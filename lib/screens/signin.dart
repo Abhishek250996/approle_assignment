fa4305b61f80ce8fb5c6caa86445cfda9352bb60
Abhishek250996 <55652397+Abhishek250996@users.noreply.github.com>
2021-06-29 23:24:22 +0530

    Update signin.dart

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -255,7 +255,7 @@ class _SigninScreenState extends State<SigninScreen> {
             height: MediaQuery.of(context).size.height/1.8,
             color: Colors.black,
             child: Padding(
-              padding: const EdgeInsets.only(top:50.0),
+              padding: const EdgeInsets.only(top:20.0),
               child: ListView(
                 children: [
                   Padding(
